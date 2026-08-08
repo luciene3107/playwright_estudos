f227689fdbeabedbffe2962c671582ca32255398
Luciene <lucienebsousa@gmail.com>
2026-08-08 17:35:08 -0300

    implementação API - delete

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,12 +1,16 @@
 import {test, expect} from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
-test('deve poder cadastrar uma nova tarefa', async ({page})=> {
+test('deve poder cadastrar uma nova tarefa', async ({page, request })=> {
+   const taskName = 'Ler um livro de typeScript';
+  
+   await request.delete('http://localhost:3333/helper/tasks/' + taskName);
+
    await page.goto('http://localhost:8080');   
    
    const inputTaskName = page.locator('input[class*=InputNewTask]');
-   //await inputTaskName.fill('Ler um livro de typeScript');
-   await inputTaskName.fill(faker.lorem.words());
+   await inputTaskName.fill(taskName);
+   //await inputTaskName.fill(faker.lorem.words());
    //await page.click('xpath=//button[contains(text(), "Create")]');
    await page.click('css=button >> text=Create');
   
