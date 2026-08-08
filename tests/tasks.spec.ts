777b0c90245e6f8ca8dc405cfc1db99629c73359
Luciene <lucienebsousa@gmail.com>
2026-08-08 17:45:31 -0300

    Implementação de comportamento

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -2,19 +2,23 @@ import {test, expect} from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
 test('deve poder cadastrar uma nova tarefa', async ({page, request })=> {
+
+   //Dado que eu tenho uma nova tarefa
    const taskName = 'Ler um livro de typeScript';
   
    await request.delete('http://localhost:3333/helper/tasks/' + taskName);
 
+   //E que eu acesso a aplicação
    await page.goto('http://localhost:8080');   
    
+   //Quando faço o cadastro desta tarefa
    const inputTaskName = page.locator('input[class*=InputNewTask]');
    await inputTaskName.fill(taskName);
    //await inputTaskName.fill(faker.lorem.words());
    //await page.click('xpath=//button[contains(text(), "Create")]');
    await page.click('css=button >> text=Create');
   
-
-   //button[contains(text(), "Create")]
-   //await page.fill('input[class*=InputNewTask]', 'Ler um livro de typeScript');
+   //Então devo ver esta tarefa na lista
+   const target = page.getByTestId('task-item');
+   await expect(target).toHaveText(taskName);
 });
