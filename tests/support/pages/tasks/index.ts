81c91aae453e0a7cdc32e487b4ca75cc78e4021a
Luciene <lucienebsousa@gmail.com>
2026-08-11 16:45:15 -0300

    implementação PO

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -1,11 +1,13 @@
-import { Page, expect } from '@playwright/test';
+import { Locator, Page, expect } from '@playwright/test';
 import { TaskModel } from '../../../fixtures/task.model';
 
 export class TaskPage {
     readonly page: Page;
+    readonly inputTaskName: Locator
 
     constructor(page: Page) {
         this.page = page;
+        this.inputTaskName = page.locator('input[class*=InputNewTask]');
     }
 
     async go() {
@@ -13,9 +15,8 @@ export class TaskPage {
     }
 
     async create(task: TaskModel) {
-
-        const inputTaskName = this.page.locator('input[class*=InputNewTask]');
-        await inputTaskName.fill(task.name);
+        
+        await this.inputTaskName.fill(task.name);
         await this.page.click('css=button >> text=Create');
     }
 
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { TaskModel } from './fixtures/task.model';
 import { deleteTaskByName, postTask } from './support/helpers';
 import { TaskPage } from './support/pages/tasks';
@@ -36,5 +36,22 @@ test('não deve permitir tarefa duplicada', async ({ page, request }) => {
 
   await taskPage.alertHaveText('Task already exists!')
 
+})
+
+test('campo obrigatório', async ({ page }) => {
+
+  const task: TaskModel = {
+    name: '',
+    is_done: false
+  }
+
+  const taskPage: TaskPage = new TaskPage(page);
+  await taskPage.go();
+  await taskPage.create(task);
+
+  
+  const validationMessage = await taskPage.inputTaskName.evaluate((e) => (e as HTMLInputElement).validationMessage);
+  expect(validationMessage).toEqual('This is a required field');
+  
 })
 
